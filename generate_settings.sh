6964e9a353bdd2ad38cb3c83a15e604068ef4aa7
scotttyso <tyscott@cisco.com>
2023-09-01 17:54:49 -0400

    Adding requirements and adding credit for wsl-sudo

--- generate_settings.sh
+++ generate_settings.sh
@@ -2,6 +2,7 @@
 apt-get install dos2unix -y
 wget https://raw.githubusercontent.com/Chronial/wsl-sudo/master/wsl-sudo.py
 mv wsl-sudo.py /usr/bin/wsl-sudo.py
+pip3 install -r requirements.txt
 chmod 755 /usr/bin/wsl-sudo.py
 cp fwdns.sh /usr/bin/fwdns.sh
 chmod 755 /usr/bin/fwdns.sh
